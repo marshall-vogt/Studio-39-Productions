0d6fcf0691d8ebfc8e5d7e8adf7ecd5677525621
Marshall Vogt <vogt_marshall@yahoo.com>
2024-01-22 22:52:48 -0800

    Added content to services, equipment, and rates pages

--- studio-39-app/src/pages/Rates.tsx
+++ studio-39-app/src/pages/Rates.tsx
@@ -2,6 +2,58 @@ export default function Rates() {
     return (
       <div className="text-white flex flex-col items-center mt-10">
         <h2 className="text-3xl">Rates</h2>
+        <div className="m-10">Studio Rental</div>
+        <ul>
+          <li>1 hour - $50</li>
+          <li>2 hour - $90</li>
+          <li>4 hour - $160</li>
+          <li>8 hour - $250</li>
+        </ul>
+        <div className="m-10">Equipment Rental</div>
+        <ul>
+          <li>2x Pioneer CDJ-3000 - $250/day (each)</li>
+          <li>Pioneer XDJ-XZ - $200/day (each)</li>
+          <li>2x JBL Jax 100 15" Speakers - $60/day (each)</li>
+          <li>2x JBL M-Pro 12" Speakers - $40/day (each)</li>
+          <li>Native Instruments Traktor S4 MkIII - $10/hr</li>
+          <li>Native Instruments Maschine MkIII - $5/hr</li>
+          <li>Moog One 16-voice Analog Synthesizer $100/hr</li>
+          <li>Moog Voyager Monophonic Analog Synthesizer - $40/hr</li>
+          <li>Roland HS-60 (Juno 106) Polyphonic Analog Synthesizer - $20/hr</li>
+          <li>Nord Lead 1 Virtual Analog Synthesizer - $15/hr</li>
+          <li>Dave Smith Tempest 6-voice Drum Machine - $25/hr</li>
+          <li>Elektron Analog Rytm MkI - $15/hr</li>
+          <li>Elektron Octatrack MkI - $15/hr</li>
+          <li>Wurlitzer 200A 64-key Electric Piano - $30/hr</li>
+          <li>Hammond B3 Organ - $25/hr</li>
+          <li>Rotating Leslie Speaker - $15/hr</li>
+          <li>Stellar CM-5 Tube Condenser Microphone - $5/hr</li>
+          <li>Vox 50th Anniversary Handwired AC15 HalfStack and 2x12 cabinet with Celestion Alnico Blue Speakers - $20/hr</li>
+          <li>Fender Jazzmaster '65 Reissue Aztec Gold - $25/hr</li>
+          <li>Rickenbacker 330 Jetglo Black - $20/hr</li>
+        </ul>
+        <div className="m-10">(Email for day rates if not specified)</div>
+        <div className="m-10">Recording Engineering Rates</div>
+        <ul>
+          <li>1 song - $100/hr</li>
+          <li>3 songs - $250/3 hrs</li>
+        </ul>
+        <div className="m-10">Mix Engineering Rates</div>
+        <ul>
+          <li>$500/day</li>
+        </ul>
+        <div className="m-10">Web Streaming</div>
+        <ul>
+          <li>$30/hr</li>
+        </ul>
+        <div className="m-10">Video Production Rates</div>
+        <ul>
+          <li>$100/hr</li>
+        </ul>
+        <div className="m-10">Beginner Lessons</div>
+        <ul>
+          <li>$50/hr (Buy 7 sessions, get 1 free)</li>
+        </ul>
         <div className="h-[18vh]"></div>
       </div>
     );
--- studio-39-app/src/pages/Services.tsx
+++ studio-39-app/src/pages/Services.tsx
@@ -2,6 +2,15 @@ export default function Services() {
     return (
       <div className="text-white flex flex-col items-center mt-10">
         <h2 className="text-3xl">Services</h2>
+        <ul>
+          <li>Studio Rentals</li>
+          <li>Equipment Rentals</li>
+          <li>Recording Engineering</li>
+          <li>Mix Engineering</li>
+          <li>Web Streaming</li>
+          <li>Video Productions</li>
+          <li>Beginner Lessons</li>
+        </ul>
         <div className="h-[18vh]"></div>
       </div>
     );
