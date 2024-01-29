6fd1a943c989c7a95edf62c36b0902861fa35d56
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2024-01-28 20:46:05 -0800

    added logo and cleaned up formatting in footer

--- studio-39-app/src/App.tsx
+++ studio-39-app/src/App.tsx
@@ -1,26 +1,25 @@
-import HomePage from './pages/HomePage';
-import NavBar from './components/NavBar';
-import { Route, Routes} from 'react-router-dom';
-import NotFound from './pages/NotFound';
-import Contact from './pages/Contact';
-import Schedule from './pages/Schedule';
-import Rates from './pages/Rates';
-import Services from './pages/Services';
-import Equipment from './pages/Equipment';
+import HomePage from "./pages/HomePage";
+import NavBar from "./components/NavBar";
+import { Route, Routes } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Contact from "./pages/Contact";
+import Schedule from "./pages/Schedule";
+import Rates from "./pages/Rates";
+import Services from "./pages/Services";
+import Equipment from "./pages/Equipment";
 
 export default function App() {
-
   return (
-      <Routes>
-        <Route path="/" element={<NavBar />}>
-          <Route index element={<HomePage />} />
-          <Route path="/schedule" element={<Schedule />} />
-          <Route path="/rates" element={<Rates />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/equipment" element={<Equipment />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+    <Routes>
+      <Route path="/" element={<NavBar />}>
+        <Route index element={<HomePage />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/rates" element={<Rates />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/equipment" element={<Equipment />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
