333025d114421253952dd8cfb2ab646b6866f599
Marshall Vogt <vogt_marshall@yahoo.com>
2024-02-06 22:29:06 -0800

    Added static files to express server

--- server/lib/server.ts
+++ server/lib/server.ts
@@ -4,6 +4,9 @@ const app: Express = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.static('../../client/dist'));
+
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
