9e5775480f64a4855c0586aceca92a0c219af150
Marshall Vogt <vogt_marshall@yahoo.com>
2024-02-06 22:55:46 -0800

    Fixed static app path

--- server/lib/server.ts
+++ server/lib/server.ts
@@ -1,10 +1,11 @@
 import express, {Express} from 'express';
+import path from 'path';
 
 const app: Express = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static('../../client/dist'));
+app.use(express.static(path.resolve('../client/dist')));
 
 
 app.listen(PORT, () => {
